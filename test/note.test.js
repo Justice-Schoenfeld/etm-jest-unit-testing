81b675e823c1a352a1ca463521f7c835dc48c931
Justice-Schoenfeld <48256685+Justice-Schoenfeld@users.noreply.github.com>
2024-05-31 12:11:20 -0600

    Got note.js unit tests working by adding global.Artifact = Artifact, thus making Artifact visible globablly in note.js file.

--- test/note.test.js
+++ test/note.test.js
@@ -1,30 +1,15 @@
-const getXML = require('./testUtility')
-const Note = require('../src/note');
-const ArtifactUtility = require('../src/artifactUtility');
-
-
-// jest.mock('Artifact', () => {
-//     return jest.fn().mockImplementation(() => {
-//       return {
-//         utils: 'hi'
-//       };
-//     });
-//   });
-
-
-
-// Mock the ParentClass
-jest.mock('../src/artifact', () => {
-    return class Artifact {
-      constructor(xml, utils) {
+class Artifact {
+    constructor(xml, utils) {
         this.xml = xml;
         this.utils = utils;
-      }
-    };
-  });
+    }
+};
 
-
-
+const getXML = require('./testUtility')
+// const Artifact = require('../src/artifact')
+global.Artifact = Artifact;
+const Note = require('../src/note');
+const ArtifactUtility = require('../src/artifactUtility');
 
 describe('Note class', () => {
     describe('good xml file', () => {
@@ -34,14 +19,29 @@ describe('Note class', () => {
         test('can read to', () => {
             expect(goodObject.to).toBe("You");
         })
+
+        test('trying to set to raises error', () => {
+            const t = () => {
+                goodObject.to = 'blah';
+            }
+            expect(t).toThrow(Error);
+            expect(t).toThrow('cannot set value of to');
+        })
+
+        test('can read from', () => {
+            expect(goodObject.from).toBe("Me");
+        })
         
     })
 
-    // describe('bad xml file', () => {
-    //     let badXml = getXML('badNote.xml');
-    //     let badObject = new Note(badXml, ArtifactUtility);
+    describe('bad xml file', () => {
+        let badXml = getXML('badNote.xml');
+        let badObject = new Note(badXml, ArtifactUtility);
 
+        test('reading to returns undefined', () => {
+            expect(badObject.to).toBe(undefined);
+        })
         
-    // })
+    })
     
 })
