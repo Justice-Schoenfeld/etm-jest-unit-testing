683d4f6d64daf7622319fc593005a283bfb2c68c
Justice-Schoenfeld <48256685+Justice-Schoenfeld@users.noreply.github.com>
2024-05-31 12:20:19 -0600

    Remove console.log in unit test

--- test/step.test.js
+++ test/step.test.js
@@ -5,7 +5,6 @@ const ArtifactUtility = require('../src/artifactUtility')
 
 describe('Step class', () => {
     let goodXml = getXML('goodNote.xml').getElementsByTagName('step')[0];
-    console.log(goodXml);
     let goodObject = new Step(goodXml, ArtifactUtility);
     test('can get step number', () => {
         expect(goodObject.number).toBe("1")
